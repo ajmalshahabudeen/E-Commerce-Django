c63007d8443c8ea2d946b281464ee142d4d4a177
Ajmal Shahabudeen <ajmalshahabudeen@gmail.com>
2023-05-26 06:00:00 +0000

    working fine -- created menu link

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'ecommerceapp'
 urlpatterns = [
-    path('',views.allProductsCategory, name = 'allProductsCategory'),
-    path('<slug:c_slug>/',views.allProductsCategory, name = 'products_by_category')
+    path('', views.allProductsCategory, name = 'allProductsCategory'),
+    path('<slug:c_slug>/', views.allProductsCategory, name = 'products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/', views.productCategoryDetail, name='productCategoryDetail')
 ]
